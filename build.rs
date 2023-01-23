6dd58ba61a5f26f51a2847e109e50f23bf8badbe
Brandon Haley <brandon.haley94@gmail.com>
2023-01-23 00:38:02 +0000

    Working GRPC server

--- build.rs
+++ build.rs
@@ -1,4 +1,15 @@
+use std::{
+    env,
+    path::PathBuf,
+};
+
 fn main() {
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    tonic_build::configure()
+        .file_descriptor_set_path(out_dir.join("route_descriptor.bin"))
+        .compile(&["proto/route.proto"], &["proto"])
+        .unwrap();
+    
     tonic_build::compile_protos("proto/route.proto")
         .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
-fn main() {
-    println!("Hello, world!");
+use futures_core::Stream;
+use tonic::transport::Server;
+use std::pin::Pin;
+use std::sync::Arc;
+use tokio::sync::mpsc;
+use tonic::{Request, Response, Status};
+use tokio_stream::wrappers::ReceiverStream;
+
+pub mod route{
+    tonic::include_proto!("route");
+
+    pub(crate) const FILE_DESCRIPTOR_SET: &[u8] = tonic::include_file_descriptor_set!("route_descriptor");
+}
+use route::route_server::{Route, RouteServer};
+use route::{User, Missive, OperationSuccess};
+
+#[derive(Debug)]
+struct RouteService;
+
+#[tonic::async_trait]
+impl Route for RouteService{
+    async fn send_message(&self, message: Request<Missive>) -> Result<Response<OperationSuccess>, Status>{
+        let reply = OperationSuccess{
+            success: true,
+            error: message.into_inner().message,
+        };
+        Ok(Response::new(reply))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let reflection = tonic_reflection::server::Builder::configure()
+        .register_encoded_file_descriptor_set(route::FILE_DESCRIPTOR_SET)
+        .build()
+        .unwrap();
+    let addr = "142.93.202.81:10000".parse().unwrap();
+
+    println!("Listening on {}", addr);
+
+    let rt = RouteService{};
+
+    let svc = RouteServer::new(rt);
+
+    Server::builder()
+    .add_service(reflection)
+    .add_service(svc)
+    .serve(addr).await?;
+
+    Ok(())
 }
